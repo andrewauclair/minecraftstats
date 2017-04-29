c631f507d56f2923ec08966845ebf349bb304ba5
andrewauclair <mightymalakai33@gmail.com>
2017-04-29 12:52:16 -0400

    RegionFileLoader will now load the chunks within a region. Still need to test that it can load multiple chunks correctly.

--- minecraftstats/src/stats/nbt/utils/RegionFileLoader.java
+++ minecraftstats/src/stats/nbt/utils/RegionFileLoader.java
@@ -1,9 +1,18 @@
 package stats.nbt.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
+
+import stats.nbt.model.ChunkModel;
+import stats.nbt.model.RegionModel;
+import stats.nbt.model.tags.TAG_Compound;
 
 public class RegionFileLoader {
 	public static final int locationsByteSize = 4096;
@@ -12,7 +21,7 @@ public class RegionFileLoader {
 	private int[] locations = new int[locationsByteSize / 4];
 	private int[] timestamps = new int[timestampsByteSize / 4];
 	
-	public void createRegionFromStream(DataInputStream input) throws IOException {
+	public RegionModel createRegionFromStream(DataInputStream input) throws IOException, DataFormatException {
 		byte[] locs = new byte[locationsByteSize];
 		input.read(locs);
 		
@@ -24,13 +33,60 @@ public class RegionFileLoader {
 		
 		intBuffer = ByteBuffer.wrap(times).asIntBuffer();
 		intBuffer.get(timestamps);
+		
+		// loop through the locations to find all the chunks and load them
+		RegionModel region = new RegionModel();
+		
+		// TODO can we just read the data and not care about the location or timestamp?
+		while (input.available() > 0) {
+			// read int for chunk size
+			int size = input.readInt();
+			
+			// read byte for compression
+			int compression = input.readByte();
+			
+			// read chunk data
+			byte[] chunkData = new byte[size - 1];
+			input.read(chunkData);
+			
+			int available = input.available();
+			
+			// default the table and read the NBT
+			Inflater decompress = new Inflater();
+			decompress.setInput(chunkData);
+			
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+			DataOutputStream output = new DataOutputStream(byteStream);
+			
+			byte[] bytes = new byte[1024];
+			
+			while (!decompress.finished()) {
+				int count = decompress.inflate(bytes);
+				output.write(bytes, 0, count);
+			}
+			
+			DataInputStream inputStream = new DataInputStream(new ByteArrayInputStream(byteStream.toByteArray()));
+			
+			TAG_Compound tag = (TAG_Compound)NBTFileHelper.readNextTag(inputStream);
+			
+			ChunkModel chunk = new ChunkModel();
+			chunk.readFromCompound(tag);
+			
+			region.addChunk(chunk);
+		}
+		
+		return region;
 	}
 	
 	public int getLocation(int x, int z) {
-		return locations[((x & 31) + (z & 31) * 32)];
+		return locations[getIndex(x, z)];
 	}
 	
 	public int getTimestamp(int x, int z) {
-		return timestamps[((x & 31) + (z & 31) * 32)];
+		return timestamps[getIndex(x, z)];
+	}
+	
+	private int getIndex(int x, int z) {
+		return (x & 31) + (z & 31) * 32;
 	}
 }
--- minecraftstats/src/stats/spec/nbt/utils/RegionFileLoaderSpecification.java
+++ minecraftstats/src/stats/spec/nbt/utils/RegionFileLoaderSpecification.java
@@ -4,34 +4,54 @@ import static org.junit.Assert.*;
 import static stats.nbt.utils.RegionFileLoader.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import stats.nbt.model.ChunkModel;
+import stats.nbt.model.RegionModel;
+import stats.nbt.model.tags.TAG.TAG_Type;
+import stats.nbt.model.tags.TAG_Compound;
+import stats.nbt.model.tags.TAG_Int;
 import stats.nbt.utils.RegionFileLoader;
 
-// The regionFileLoader class will parse through a region file and create a 
-// RegionModel out of all the chunks found in it
-
-// load 3 chunks and assert that they were read with the correct data
 public class RegionFileLoaderSpecification {
 	private RegionFileLoader loader;
 	private int x;
 	private int z;
+	private byte[] chunkData = new byte[100];
+	TAG_Compound payload;
+	private int payloadSize;
 	
-	// chunk: (16, 8)
-	// location and timestamp: 272
-	// chunk (16, 8) at: 8192
+	// TODO Expand this test to cover 2 loaded chunks
 	
 	@Before
-	public void setup() {
+	public void setup() throws IOException {
 		loader = new RegionFileLoader();
 		x = 16;
 		z = 8;
+		payload = new TAG_Compound("Root");
+		TAG_Compound level = new TAG_Compound(ChunkModel.levelTagName);
+		level.addTAG(new TAG_Int(ChunkModel.xPosTagName, x));
+		level.addTAG(new TAG_Int(ChunkModel.zPosTagName, z));
+		payload.addTAG(level);
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream output = new DataOutputStream(byteStream);
+		output.writeByte(TAG_Type.TAG_Compound.getValue());
+		payload.writeToStream(output, true);
+		
+		Deflater compress = new Deflater();
+		compress.setInput(byteStream.toByteArray());
+		compress.finish();
+		payloadSize = compress.deflate(chunkData);
 	}
 	
 	@Test
@@ -41,26 +61,31 @@ public class RegionFileLoaderSpecification {
 	}
 	
 	@Test
-	public void ShouldReadFromStream() throws IOException {
+	public void ShouldReadFromStream() throws IOException, DataFormatException {
 		int[] locations = new int[locationsByteSize / 4];
 		locations[272] = 0x2345;
 
 		int[] timestamps = new int[timestampsByteSize / 4];
 		timestamps[272] = 0x5678;
 		
-		ByteBuffer byteBuffer = ByteBuffer.allocate(locationsByteSize + timestampsByteSize);
+		ByteBuffer byteBuffer = ByteBuffer.allocate(locationsByteSize + timestampsByteSize + 4);
         IntBuffer intBuffer = byteBuffer.asIntBuffer();
         intBuffer.put(locations);
         intBuffer.put(timestamps);
+        intBuffer.put(payloadSize + 1);
         
         byte[] array = byteBuffer.array();
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(array.length + payloadSize + 1);
+        byteBuffer2.put(array);
+        byteBuffer2.put((byte)2);
+        byteBuffer2.put(chunkData, 0, payloadSize);
         
-        DataInputStream input = new DataInputStream(new ByteArrayInputStream(array));
+        DataInputStream input = new DataInputStream(new ByteArrayInputStream(byteBuffer2.array()));
         
-        // TODO This needs to return a RegionModel and we need to assert that it has 1 chunk
-        loader.createRegionFromStream(input);
+        RegionModel region = loader.createRegionFromStream(input);
         
         assertEquals(0x2345, loader.getLocation(16, 8));
         assertEquals(0x5678, loader.getTimestamp(16, 8));
+        assertNotNull(region.getChunk(x, z));
 	}
 }
